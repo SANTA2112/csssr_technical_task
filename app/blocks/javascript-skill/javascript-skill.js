af06a8bb5723dbc21b40f4445e16800e99354c73
Santa <s.yushmanov777@gmail.com>
2018-11-12 07:54:59 +0700

    add script for slider

---
+++ app/blocks/javascript-skill/javascript-skill.js
@@ -0,0 +1,12 @@
+const progresssBar = document.querySelector('.javascript-skill__progressbar');
+const marker = document.querySelector('.javascript-skill__progressbar-mark');
+const inputRange = document.querySelector('.javascript-skill__range');
+const wbar = progresssBar.clientWidth;
+
+inputRange.max = wbar;
+inputRange.value = wbar / 100 * 30;
+marker.style.left = `${inputRange.value - 8}px`;
+
+inputRange.addEventListener('input', e => marker.style.left = `${e.target.value - 8}px`);
+
+
